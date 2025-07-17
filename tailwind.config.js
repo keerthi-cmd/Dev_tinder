3fe666aa123885130beee9ae40fa793b37cf8f69
Keerthi <keerthi.appasani1@gmail.com>
2025-07-18 08:34:00 +1000

    added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark"], // use built-in themes
+    darkTheme: "dark", // specify which theme to use in dark mode
+  },
+  darkMode: "class", // enables manual control via "dark" class
 };
